2a22e83ff68d9c41cb18f85f33bd1d33d89f1dc7
Alan Kofi Safo Ofori <144915821+oforias@users.noreply.github.com>
2025-07-15 19:11:15 +0000

    Delete shoppingapp directory

--- shoppingapp/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'pages/home_page.dart';
-
-void main() => runApp(
-  MaterialApp(
-    initialRoute: '/',
-    routes: {'/': (context) => ECommerceHomePage()},
-  ),
-);
